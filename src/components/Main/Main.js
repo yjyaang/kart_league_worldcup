c077d2a6027f251cef266a674bad223a9dd005f7
yjyaang <topbuty97@gmail.com>
2022-12-29 13:29:43 +0900

    video_seperate

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,33 +1,7 @@
 import React, { useState, useEffect } from 'react'
 // import styled from "styled-components"
 import { FlexBox, SelectArea, SelectBtn, RoundCounter } from './Style';
-// import Video1 from '../../video/Video1';
-// import Video2 from '../../video/Video2';
-// import Video3 from '../../video/Video3';
-// import Video4 from '../../video/Video4';
-
-const items = [
-    {
-        name: '에볼루션 결승 에결\n문호준vs유영혁',
-        src: <iframe src="https://www.youtube.com/embed/Jm5CtdZz7tY" title="YouTube video player" className='youtube'
-        frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-    },
-    {
-        name: '2022 시즌 2 플레이오프 에결\n유창현vs노준현',
-        src: <iframe src="https://www.youtube.com/embed/eP1eEJHnw_g" title="YouTube video player" className='youtube'
-        frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-    },
-    {
-        name: '2020 시즌 1 개인전 결승 SET 1\n문호준',
-        src: <iframe src="https://www.youtube.com/embed/ypPIwE9q7XI" title="YouTube video player" className='youtube'
-        frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-    },
-    {
-        name: '2019 시즌 1 개인전 결승\n문호준vs박인수',
-        src: <iframe src="https://www.youtube.com/embed/G1R8tJMEAR4" title="YouTube video player" className='youtube'
-        frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-    }
-]
+import {items} from '../../video/Videos'
 
 const Main = () => {
     const [clips, setClips] = useState([]);
---
+++ src/video/Videos.js
@@ -0,0 +1,22 @@
+export const items = [
+    {
+        name: '에볼루션 결승 에결\n문호준vs유영혁',
+        src: <iframe src="https://www.youtube.com/embed/Jm5CtdZz7tY" title="YouTube video player" className='youtube'
+        frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    },
+    {
+        name: '2022 시즌 2 플레이오프 에결\n유창현vs노준현',
+        src: <iframe src="https://www.youtube.com/embed/eP1eEJHnw_g" title="YouTube video player" className='youtube'
+        frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    },
+    {
+        name: '2020 시즌 1 개인전 결승 SET 1\n문호준',
+        src: <iframe src="https://www.youtube.com/embed/ypPIwE9q7XI" title="YouTube video player" className='youtube'
+        frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    },
+    {
+        name: '2019 시즌 1 개인전 결승\n문호준vs박인수',
+        src: <iframe src="https://www.youtube.com/embed/G1R8tJMEAR4" title="YouTube video player" className='youtube'
+        frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    }
+]
